4569016c64307fe9e0dd2ee91d79f2244ba46184
Sven Verdoolaege <skimo@kotnet.org>
2017-01-25 15:42:21 +0100

    specialize isl_space_replace to isl_space_replace_params
    isl_space_replace is only ever used to replace parameters.
    Rename isl_space_replace to isl_space_replace_params and
    drop the redundant type argument.
    
    Signed-off-by: Sven Verdoolaege <sven.verdoolaege@gmail.com>

--- isl_space_private.h
+++ isl_space_private.h
@@ -53,8 +53,8 @@ __isl_give isl_space *isl_space_flatten(__isl_take isl_space *dim);
 __isl_give isl_space *isl_space_flatten_domain(__isl_take isl_space *dim);
 __isl_give isl_space *isl_space_flatten_range(__isl_take isl_space *dim);
 
-__isl_give isl_space *isl_space_replace(__isl_take isl_space *dst,
-	enum isl_dim_type type, __isl_keep isl_space *src);
+__isl_give isl_space *isl_space_replace_params(__isl_take isl_space *dst,
+	__isl_keep isl_space *src);
 
 __isl_give isl_space *isl_space_lift(__isl_take isl_space *dim, unsigned n_local);
 
