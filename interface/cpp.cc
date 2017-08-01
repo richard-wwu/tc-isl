9e2e0a4d97db601c3ccb71ae9429eb3f58fba5d1
Sven Verdoolaege <sven.verdoolaege@gmail.com>
2017-08-01 21:50:04 +0200

    interface/cpp.cc: cpp_generator::print_callback_local: break up osprintf call
    In particular, perform a separate osprintf call for each line.
    This brings the arguments closer to the corresponding conversion specifiers
    and makes it easier to make adjustments, such as those in the next commit.
    
    Signed-off-by: Sven Verdoolaege <sven.verdoolaege@gmail.com>

--- interface/cpp.cc
+++ interface/cpp.cc
@@ -965,13 +965,16 @@ void cpp_generator::print_callback_local(ostream &os, ParmVarDecl *param) {
 
 	osprintf(os, "  auto %s_p = &%s;\n", pname.c_str(), pname.c_str());
 	osprintf(os,
-		 "  auto %s_lambda = [](%s) -> %s {\n"
-		 "    auto *func = *static_cast<const %s **>(arg_%s);\n"
+		 "  auto %s_lambda = [](%s) -> %s {\n",
+		 pname.c_str(), c_args.c_str(), rettype.c_str());
+	osprintf(os,
+		 "    auto *func = *static_cast<const %s **>(arg_%s);\n",
+		 cpp_args.c_str(), last_idx.c_str());
+	osprintf(os,
 		 "    stat ret = (*func)(%s);\n"
 		 "    return isl_stat(ret);\n"
 		 "  };\n",
-		 pname.c_str(), c_args.c_str(), rettype.c_str(),
-		 cpp_args.c_str(), last_idx.c_str(), call_args.c_str());
+		 call_args.c_str());
 }
 
 /* An array listing functions that must be renamed and the function name they
