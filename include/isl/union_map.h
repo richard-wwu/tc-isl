2c7fb2bbfcd18b020651c13dd85df62fd4a77ef0
Sven Verdoolaege <sven.verdoolaege@gmail.com>
2017-10-13 14:59:37 +0200

    add isl_union_map_intersect_domain_wrapped_domain
    This function allows the wrapped domain to be intersected directly,
    without having to first modify the union map to expose the domain.
    
    Signed-off-by: Sven Verdoolaege <sven.verdoolaege@gmail.com>

--- include/isl/union_map.h
+++ include/isl/union_map.h
@@ -147,6 +147,8 @@ __isl_give isl_union_map *isl_union_map_intersect_range(
 	__isl_take isl_union_map *umap, __isl_take isl_union_set *uset);
 __isl_give isl_union_map *isl_union_map_intersect_range_factor_range(
 	__isl_take isl_union_map *umap, __isl_take isl_union_map *factor);
+__isl_give isl_union_map *isl_union_map_intersect_domain_wrapped_domain(
+	__isl_take isl_union_map *umap, __isl_take isl_union_set *domain);
 
 __isl_export
 __isl_give isl_union_map *isl_union_map_subtract_domain(
