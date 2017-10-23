3b141b69fbf5e82f6e580b63fe7e63a043a25bba
Oleksandr Zinenko <ftynse@gmail.com>
2017-10-23 14:53:51 -0400

    export point_is_void, set_is_singleton

--- include/isl/point.h
+++ include/isl/point.h
@@ -21,6 +21,7 @@ __isl_give isl_point *isl_point_zero(__isl_take isl_space *dim);
 __isl_give isl_point *isl_point_copy(__isl_keep isl_point *pnt);
 __isl_null isl_point *isl_point_free(__isl_take isl_point *pnt);
 
+__isl_export
 __isl_give isl_val *isl_point_get_coordinate_val(__isl_keep isl_point *pnt,
 	enum isl_dim_type type, int pos);
 __isl_give isl_point *isl_point_set_coordinate_val(__isl_take isl_point *pnt,
@@ -34,6 +35,7 @@ __isl_give isl_point *isl_point_sub_ui(__isl_take isl_point *pnt,
 	enum isl_dim_type type, int pos, unsigned val);
 
 __isl_give isl_point *isl_point_void(__isl_take isl_space *dim);
+__isl_export
 isl_bool isl_point_is_void(__isl_keep isl_point *pnt);
 
 __isl_give isl_printer *isl_printer_print_point(
