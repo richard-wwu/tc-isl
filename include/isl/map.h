4f64ce385fbe41ab860630657d4b7dca6a777ca5
Oleksandr Zinenko <git@ozinenko.com>
2018-01-15 16:22:29 +0100

    export isl_map_from_basic_map as __isl_constructor
    ae4571e03 exported isl_map_from_basic_map as a function, but it is in
    fact a constructor.  Because it does not loose information, export it as
    constructor instead, according to the bindings guidelines.

--- include/isl/map.h
+++ include/isl/map.h
@@ -550,7 +550,7 @@ __isl_export
 __isl_give isl_map *isl_map_range_map(__isl_take isl_map *map);
 __isl_export
 __isl_give isl_map *isl_set_wrapped_domain_map(__isl_take isl_set *set);
-__isl_export
+__isl_constructor
 __isl_give isl_map *isl_map_from_basic_map(__isl_take isl_basic_map *bmap);
 __isl_export
 __isl_give isl_map *isl_map_from_domain(__isl_take isl_set *set);
