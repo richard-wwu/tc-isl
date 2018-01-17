4ff8224653ba5fdea2fa355f8c984263d940fb39
Nicolas Vasilache <nicolas.vasilache@gmail.com>
2018-01-17 13:30:37 -0800

    Add LLVM-5.0 support, make binding generation optional and ship bindings as part of our branch for now

--- interface/extract_interface.cc
+++ interface/extract_interface.cc
@@ -1,18 +1,18 @@
 /*
  * Copyright 2011 Sven Verdoolaege. All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
- * 
+ *
  *    1. Redistributions of source code must retain the above copyright
  *       notice, this list of conditions and the following disclaimer.
- * 
+ *
  *    2. Redistributions in binary form must reproduce the above
  *       copyright notice, this list of conditions and the following
  *       disclaimer in the documentation and/or other materials provided
  *       with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY SVEN VERDOOLAEGE ''AS IS'' AND ANY
  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
@@ -24,12 +24,12 @@
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  * The views and conclusions contained in the software and documentation
  * are those of the authors and should not be interpreted as
  * representing official policies, either expressed or implied, of
  * Sven Verdoolaege.
- */ 
+ */
 
 #include "isl_config.h"
 
@@ -362,16 +362,26 @@ static void set_lang_defaults(CompilerInstance *Clang)
 	PreprocessorOptions &PO = Clang->getPreprocessorOpts();
 	TargetOptions &TO = Clang->getTargetOpts();
 	llvm::Triple T(TO.Triple);
+#if LLVM_VERSION_MAJOR>=5
+	CompilerInvocation::setLangDefaults(Clang->getLangOpts(), InputKind::C, T, PO,
+					    LangStandard::lang_unspecified);
+#else
 	CompilerInvocation::setLangDefaults(Clang->getLangOpts(), IK_C, T, PO,
 					    LangStandard::lang_unspecified);
+#endif
 }
 
 #else
 
 static void set_lang_defaults(CompilerInstance *Clang)
 {
+#if LLVM_VERSION_MAJOR>=5
+	CompilerInvocation::setLangDefaults(Clang->getLangOpts(), InputKind::C,
+					    LangStandard::lang_unspecified);
+#else
 	CompilerInvocation::setLangDefaults(Clang->getLangOpts(), IK_C,
 					    LangStandard::lang_unspecified);
+#endif
 }
 
 #endif
