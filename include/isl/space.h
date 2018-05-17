d58a330de9dc64e657f6c5bb568207ff50ff5878
Sven Verdoolaege <sven.verdoolaege@gmail.com>
2018-05-17 12:23:45 +0200

    drop isl_space_find_dim_by_name export

--- include/isl/space.h
+++ include/isl/space.h
@@ -72,7 +72,6 @@ __isl_give isl_id *isl_space_get_dim_id(__isl_keep isl_space *dim,
 
 int isl_space_find_dim_by_id(__isl_keep isl_space *dim, enum isl_dim_type type,
 	__isl_keep isl_id *id);
-__isl_export
 int isl_space_find_dim_by_name(__isl_keep isl_space *space,
 	enum isl_dim_type type, const char *name);
 
