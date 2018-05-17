4cec09675fa91d31cdfbfe1f31210ee5bec3f7d4
Sven Verdoolaege <sven.verdoolaege@gmail.com>
2018-05-17 12:26:27 +0200

    drop isl_set_set_dim_name export

--- include/isl/set.h
+++ include/isl/set.h
@@ -75,7 +75,6 @@ isl_bool isl_set_has_dim_name(__isl_keep isl_set *set,
 	enum isl_dim_type type, unsigned pos);
 const char *isl_set_get_dim_name(__isl_keep isl_set *set,
 	enum isl_dim_type type, unsigned pos);
-__isl_export
 __isl_give isl_set *isl_set_set_dim_name(__isl_take isl_set *set,
 	enum isl_dim_type type, unsigned pos, const char *s);
 
