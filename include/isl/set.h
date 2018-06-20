3bd7c3bdafac27997b6a9d8bf81ad406f63b069d
Sven Verdoolaege <sven.verdoolaege@gmail.com>
2018-06-20 12:40:51 +0200

    export isl_set_from_multi_aff

--- include/isl/set.h
+++ include/isl/set.h
@@ -551,6 +551,7 @@ __isl_give isl_basic_set *isl_basic_set_from_constraint_matrices(
 __isl_give isl_basic_set *isl_basic_set_from_multi_aff(
 	__isl_take isl_multi_aff *ma);
 
+__isl_overload
 __isl_give isl_set *isl_set_from_multi_aff(__isl_take isl_multi_aff *ma);
 
 __isl_give isl_mat *isl_basic_set_reduced_basis(__isl_keep isl_basic_set *bset);
