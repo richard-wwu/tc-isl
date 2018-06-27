9d7f29c55ec8b2fda22b3b79d8cc26e3adcdaea9
Sven Verdoolaege <sven.verdoolaege@gmail.com>
2018-06-27 16:39:22 +0200

    drop isl_space_has_tuple_id export

--- include/isl/space.h
+++ include/isl/space.h
@@ -55,7 +55,6 @@ __isl_give isl_space *isl_space_set_tuple_id(__isl_take isl_space *dim,
 	enum isl_dim_type type, __isl_take isl_id *id);
 __isl_give isl_space *isl_space_reset_tuple_id(__isl_take isl_space *dim,
 	enum isl_dim_type type);
-__isl_export
 isl_bool isl_space_has_tuple_id(__isl_keep isl_space *dim,
 	enum isl_dim_type type);
 __isl_export
